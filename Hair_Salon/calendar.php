46f52bfb7767dad73330bea4ae76a96358591e2d
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-08-19 17:17:19 +0900

    calendar

--- Hair_Salon/calendar.php
+++ Hair_Salon/calendar.php
@@ -1,1 +1,87 @@
-<?php echo "hello";?>
+<?php
+
+// 今の年月を取得
+$year = date('y');
+$month = date('m');
+
+// 月末日までを取得
+$last_day = date('d', mktime(0, 0, 0, $month + 1, 0, $year));
+
+$calendar = array();
+$j = 0;
+
+for ($i = 1; $i < $last_day + 1; $i++) {
+  $week = date('w', mktime(0, 0, 0, $month, $i, $year));
+  if ($i == 1) {
+    for ($s = 1; $s <= $week; $s++) {
+      $calendar[$j]['day'] = ' ';
+      $j++;
+    }
+  }
+  $calendar[$j]['day'] = $i;
+  $j++;
+  if ($i == $last_day) {
+    for ($e = 1; $e <= 6 - $week; $e++) {
+      $calendar[$j]['day'] = ' ';
+      $j++;
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="jp">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>予約カレンダー</title>
+</head>
+<body>
+
+  20<?php echo $year; ?> 年 <select name="month"></select><?php echo $month; ?> 月のカレンダー
+
+  <table>
+    <tr>
+      <th>日</th>
+      <th>月</th>
+      <th>火</th>
+      <th>水</th>
+      <th>木</th>
+      <th>金</th>
+      <th>土</th>
+    </tr>
+
+    <tr>
+    <?php $cnt = 0; ?>
+    <?php foreach ($calendar as $key => $value): ?>
+      <td>
+      <?php $cnt++; ?>
+      <?php echo $value['day']; ?>
+      </td>
+    <?php if ($cnt == 7): ?>
+    </tr>
+
+    <tr>
+    <?php $cnt = 0; ?>
+    <?php endif; ?>
+
+    <?php  endforeach; ?>
+    </tr>
+  </table>
+
+  <style type="text/css">
+    table {
+      width: 100%;
+    }
+    table th {
+      background: #eee;
+    }
+    table th,
+    table td {
+      border: 1px solid #ccc;
+      text-align: center;
+      padding: 5px;
+    }
+  </style>
+</body>
+
+</html>
