16f85f6cb4256db63c966d416678b70f43569d8f
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-08-19 19:24:20 +0900

    calendar2

--- Hair_Salon/calendar.php
+++ Hair_Salon/calendar.php
@@ -2,26 +2,46 @@
 
 // 今の年月を取得
 $year = date('y');
-$month = date('m');
+$month = date('n');
+
+// 来月の設定
+$next_month = $month + 1;
 
 // 月末日までを取得
-$last_day = date('d', mktime(0, 0, 0, $month + 1, 0, $year));
+$last_day = date('j', mktime(0, 0, 0, $month + 1, 0, $year));
 
 $calendar = array();
 $j = 0;
 
+// 月末までループ
 for ($i = 1; $i < $last_day + 1; $i++) {
+
+  // 曜日を取得             時, 分, 秒, 月,    日, 年
   $week = date('w', mktime(0, 0, 0, $month, $i, $year));
+
+  // 1日の時
   if ($i == 1) {
+
+    // 1日目の曜日までループ
     for ($s = 1; $s <= $week; $s++) {
+
+      // 空文字をいれる
       $calendar[$j]['day'] = ' ';
       $j++;
     }
   }
+
+  // 配列に日付をセット
   $calendar[$j]['day'] = $i;
   $j++;
+
+  // 月末日の時
   if ($i == $last_day) {
+
+    // 月末日からループ
     for ($e = 1; $e <= 6 - $week; $e++) {
+
+      // 空文字をいれる
       $calendar[$j]['day'] = ' ';
       $j++;
     }
@@ -37,7 +57,9 @@ for ($i = 1; $i < $last_day + 1; $i++) {
 </head>
 <body>
 
-  20<?php echo $year; ?> 年 <select name="month"></select><?php echo $month; ?> 月のカレンダー
+  <div class = "header">
+    20<?php echo $year; ?> 年 <?php echo $month; ?> 月
+  </div>
 
   <table>
     <tr>
@@ -51,13 +73,13 @@ for ($i = 1; $i < $last_day + 1; $i++) {
     </tr>
 
     <tr>
-    <?php $cnt = 0; ?>
-    <?php foreach ($calendar as $key => $value): ?>
-      <td>
-      <?php $cnt++; ?>
-      <?php echo $value['day']; ?>
-      </td>
-    <?php if ($cnt == 7): ?>
+      <?php $cnt = 0; ?>
+      <?php foreach ($calendar as $key => $value): ?>
+        <td>
+        <?php $cnt++; ?>
+        <?php echo $value['day']; ?>
+        </td>
+      <?php if ($cnt == 7): ?>
     </tr>
 
     <tr>
@@ -68,7 +90,17 @@ for ($i = 1; $i < $last_day + 1; $i++) {
     </tr>
   </table>
 
+  <div>
+    <?php echo $next_month; ?>
+  </div>
+
   <style type="text/css">
+    .header {
+      height: 50px;
+      display: flex;
+      align-items: flex-end;
+      justify-content: center;
+    }
     table {
       width: 100%;
     }
