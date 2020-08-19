51a616ab5787ab56791ab71def7d4c0ae4bac42b
hiro0116 <67264472+hiro0116@users.noreply.github.com>
2020-08-19 15:02:50 +0900

    php2

--- Hair_Salon/sent.php
+++ Hair_Salon/sent.php
@@ -3,15 +3,26 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="sent.css">
   <title>お問い合わせフォーム</title>
 </head>
 <body>
-  <h1>お問い合わせありがとうございます</h1>
-  <div>
-    <h2>入力内容</h2>
-    <div>メールアドレス
-      <?php echo $_POST['email']; ?>
+  <div class = "contact">
+    <h1>お問い合わせありがとうございます</h1>
+    <div class = "form-result">
+      <div class = "result-title">入力内容</div>
+      <div class = "result-item">■ メールアドレス</div>
+        <br>
+        <?php echo $_POST['email']; ?>
+      <div class = "result-item">■ 件名</div>
+        <br>
+        <?php echo $_POST['subject']; ?>
+      <div class = "result-item">■ お問い合わせ内容</div>
+        <br>
+        <?php echo $_POST['form']; ?>
+    </div>
+    <div class = "back-btn">
+      <a href="index.php" class = "back">戻る</a>
     </div>
   </div>
 </body>
